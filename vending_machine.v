17f3248ee5ba2d7cb3fcb16f9037267d501479d2
Isha Rani <164611509+isha-girdhar@users.noreply.github.com>
2025-12-18 12:36:53 +0530

    vending_machine.v

--- vending_machine.v
+++ vending_machine.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10.07.2025 22:51:59
+// Create Date: 10.07.2025 21:40:30
 // Design Name: 
 // Module Name: vending_machine
 // Project Name: 
@@ -20,7 +20,33 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module vending_machine(
+module vending_machine( input clk,
+input reset,
+input coin_5,
+input coin_10,
+output dispense,
+output[1:0] current_state
 
     );
+    parameter S0=0, S1=1, S2=2, S3=3;
+    reg[1:0]state, next_state;
+   
+    always@(posedge clk or posedge reset) begin
+    if(reset)
+    state<=S0;
+    else
+    state<=next_state;
+    end
+    always@(*) begin
+    case(state)
+    S0: next_state=coin_5?S1:coin_10?S2:S0;
+    S1: next_state=coin_5?S2:coin_10?S3:S1;
+    S2: next_state=(coin_5||coin_10)?S3:S2;
+    S3: next_state=S0;
+    default:next_state= S0;
+    endcase
+    end
+    
+    assign dispense=((state==S2)&&(coin_5||coin_10))||(state==S3);
+    assign current_state=state;
 endmodule
